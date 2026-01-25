80e6d2cc777d83ec030866cda1ea878244814805
robk <robk@localhost>
2026-01-25 15:41:24 +0000

    [HL-47] Testing ASG for aws-java1

--- java-app1/src/main/java/com/example/demo/DemoApplication.java
+++ java-app1/src/main/java/com/example/demo/DemoApplication.java
@@ -26,7 +26,7 @@ public final class DemoApplication {
  */
 @GetMapping("/")
 public String home() {
-    String backgroundColor = "#3ba8d3"; // Change this to test deployments
+    String backgroundColor = "#38a0c9"; // Change this to test deployments
     return "<html><body style='background-color:"
             + backgroundColor
             + "; font-family: Arial; padding: 40px;'>"
