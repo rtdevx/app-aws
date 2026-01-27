cb7d71cd4165947bf71f992a4f805d8dd852e960
robk <robk@localhost>
2026-01-27 10:45:03 +0000

    [HL-46] Testing if deployment is triggering asg refresh

--- jvx/src/main/java/com/example/demo/DemoApplication.java
+++ jvx/src/main/java/com/example/demo/DemoApplication.java
@@ -26,7 +26,7 @@ public final class DemoApplication {
  */
 @GetMapping("/")
 public String home() {
-    String backgroundColor = "#2b8aaf"; // Change this to test deployments
+    String backgroundColor = "#37a9d6"; // Change this to test deployments
     return "<html><body style='background-color:"
             + backgroundColor
             + "; font-family: Arial; padding: 40px;'>"
