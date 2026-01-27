6dd4eadad50131c88af3021627191ce4b31b4505
robk <robk@localhost>
2026-01-27 12:15:05 +0000

    [HL-46] Deploying.

--- jvx/src/main/java/com/example/demo/DemoApplication.java
+++ jvx/src/main/java/com/example/demo/DemoApplication.java
@@ -26,7 +26,7 @@ public final class DemoApplication {
  */
 @GetMapping("/")
 public String home() {
-    String backgroundColor = "#2d8bb1"; // Change this to test deployments
+    String backgroundColor = "#3eb8e9"; // Change this to test deployments
     return "<html><body style='background-color:"
             + backgroundColor
             + "; font-family: Arial; padding: 40px;'>"
