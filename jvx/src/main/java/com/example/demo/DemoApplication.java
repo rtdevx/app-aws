6cfff3ee1e07b089fd841f63542ab55e53fccf26
robk <robk@localhost>
2026-02-22 11:11:50 +0000

    [HL-54] Testing Java App.

--- jvx/src/main/java/com/example/demo/DemoApplication.java
+++ jvx/src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @SpringBootApplication
 @RestController
+@SuppressWarnings("checkstyle:FinalClass")
 public class DemoApplication {
 
     /**
