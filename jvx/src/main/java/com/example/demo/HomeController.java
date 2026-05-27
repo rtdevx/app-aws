7518265ff888229ec9636e68a4cee8f68b70dd6e
robk <robk@localhost>
2026-05-27 14:15:15 +0100

    [HL-70] Testing Java App - Colour change.

--- jvx/src/main/java/com/example/demo/HomeController.java
+++ jvx/src/main/java/com/example/demo/HomeController.java
@@ -16,7 +16,7 @@ public class HomeController {
      */
     @GetMapping("/")
     public String home() {
-        String backgroundColor = "#1d6ade";
+        String backgroundColor = "#de1d2d";
         return "<html><body style='background-color:"
                 + backgroundColor
                 + "; font-family: Arial; padding: 40px;'>"
