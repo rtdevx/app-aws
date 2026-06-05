d319588ab53790204e8b686c6746b4641c0d91fe
robk <robk@localhost>
2026-06-05 17:25:08 +0100

    [HL-73] FEAT: Multi-Region enablement

--- jvx/src/main/java/com/example/demo/HomeController.java
+++ jvx/src/main/java/com/example/demo/HomeController.java
@@ -16,7 +16,7 @@ public class HomeController {
      */
     @GetMapping("/")
     public String home() {
-        String backgroundColor = "#2dde1d";
+        String backgroundColor = "#bbde1d";
         return "<html><body style='background-color:"
                 + backgroundColor
                 + "; font-family: Arial; padding: 40px;'>"
